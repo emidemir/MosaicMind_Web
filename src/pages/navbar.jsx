c6ffd595ed538fadd5ebdd48d55fc0dd68897813
Emirhan Utku <emirhan.2521demir@gmail.com>
2024-05-02 20:06:36 +0300

    Navbar & Percentage is refined

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -5,7 +5,7 @@ export default function navbar(){
 	return (
 		<nav className="navbar">
 			<Link className='text-link' to="/machines"><div className='text-3'>Devices</div></Link>
-			<Link className='text-link' to="/"><div className='text-3'>LogOut</div></Link>
+			<Link className='text-link' to="/"><div className='text-3'>Log Out</div></Link>
 		</nav>
 	)
 }
