98f7a69a4728cb03aa08ca4f822b3e3e7cf344f1
Emirhan Utku <emirhan.2521demir@gmail.com>
2024-05-02 19:54:01 +0300

    LogOut

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -5,7 +5,7 @@ export default function navbar(){
 	return (
 		<nav className="navbar">
 			<Link className='text-link' to="/machines"><div className='text-3'>Devices</div></Link>
-			<Link className='text-link' to="/signin"><div className='text-3'>Profile</div></Link>
+			<Link className='text-link' to="/"><div className='text-3'>LogOut</div></Link>
 		</nav>
 	)
 }
